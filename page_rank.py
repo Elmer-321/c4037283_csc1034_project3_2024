05a59c0566e2fb5a682bfd33b96dd6836db64891
Elmer-321 <tomelmer99@gmail.com>
2024-12-09 14:45:23 +0000

    Added stochastic_page_rank

--- page_rank.py
+++ page_rank.py
@@ -2,22 +2,15 @@ import sys
 import os
 import time
 import argparse
+import random
 from progress import Progress
 
-G = {}
 
+# loads a graph from a text file
 def load_graph(args):
-    """Load graph from text file
-
-    Parameters:
-    args -- arguments named tuple
-
-    Returns:
-    A dict mapling a URL (str) to a list of target URLs (str).
-    """
-
     file = open("school_web2024-1.txt")
     contains = []
+    G = {}
 
     # Iterate through the file line by line
     for line in file:
@@ -28,8 +21,9 @@ def load_graph(args):
             contains.append(node)
         else:
             G[node].append(target)
+    return G
 
-
+# Prints the amount of nodes and edges in a graph
 def print_stats(graph):
     print("Nodes =", len(graph))
     edges = 0
@@ -37,22 +31,24 @@ def print_stats(graph):
         edges += len(i)
     print("Edges =", edges)
 
-
 def stochastic_page_rank(graph, args):
-    """Stochastic PageRank estimation
+    
+    c = 0
+    visited = []
+    totals = {}
+    node_key = random.choice(list(graph.keys()))
+    while c < args[0]:
+        node_values = graph[node_key]
+        if len(node_values) > 0:
+            node_key = random.choice(node_values)
+            visited.append(node_key)
+        else:
+            node_key = random.choice(list(graph.keys()))
+        c += 1
+    for i in graph:
+        totals[i] = visited.count(i)
 
-    Parameters:
-    graph -- a graph object as returned by load_graph()
-    args -- arguments named tuple
-
-    Returns:
-    A dict that assigns each page its hit frequency
-
-    This function estimates the Page Rank by counting how frequently
-    a random walk that starts on a random node will after n_steps end
-    on each node of the given graph.
-    """
-    pass
+    return totals[args[1]]
 
 
 def distribution_page_rank(graph, args):
@@ -80,20 +76,24 @@ parser.add_argument('-r', '--repeats', type=int, default=1_000_000, help="number
 parser.add_argument('-s', '--steps', type=int, default=100, help="number of steps a walker takes")
 parser.add_argument('-n', '--number', type=int, default=20, help="number of results shown")
 
-if __name__ == '__main__':
-    args = parser.parse_args()
-    algorithm = distribution_page_rank if args.method == 'distribution' else stochastic_page_rank
+# if __name__ == '__main__':
+#    args = parser.parse_args()
+#    algorithm = distribution_page_rank if args.method == 'distribution' else stochastic_page_rank
+#
+#    graph = load_graph(args)
+#
+#    print_stats(graph)
+#
+#    start = time.time()
+#    ranking = algorithm(graph, args)
+#    stop = time.time()
+#    time = stop - start
+#
+#    top = sorted(ranking.items(), key=lambda item: item[1], reverse=True)
+#    sys.stderr.write(f"Top {args.number} pages:\n")
+#    print('\n'.join(f'{100*v:.2f}\t{k}' for k,v in top[:args.number]))
+#    sys.stderr.write(f"Calculation took {time:.2f} seconds.\n")
 
-    graph = load_graph(args)
 
-    print_stats(graph)
-
-    start = time.time()
-    ranking = algorithm(graph, args)
-    stop = time.time()
-    time = stop - start
-
-    top = sorted(ranking.items(), key=lambda item: item[1], reverse=True)
-    sys.stderr.write(f"Top {args.number} pages:\n")
-    print('\n'.join(f'{100*v:.2f}\t{k}' for k,v in top[:args.number]))
-    sys.stderr.write(f"Calculation took {time:.2f} seconds.\n")
+g = load_graph((0))
+print(stochastic_page_rank(g, (10000, "http://www.ncl.ac.uk/computing/")))
